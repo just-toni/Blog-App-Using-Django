45f2d095e37a10f6676cd3d4c012aa70f38404dd
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-11-15 16:19:23 +0100

    Blog in Django

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -13,7 +14,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default= timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
@@ -21,5 +22,5 @@ class Post(models.Model):
     class Meta:
         ordering = ('-publish',)
 
-    def _str_(self):
+    def __str__(self):
         return self.title
